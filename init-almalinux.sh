b59f62bf33c7a6b78fe1d1a8f60432aecca2c7d1
linjicong <932997844@qq.com>
2023-11-29 10:41:59 +0800

    +

--- init-almalinux.sh
+++ init-almalinux.sh
@@ -7,6 +7,6 @@ sudo setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 
 # 配置sshd
-sed -i 's/#Port 22/Port 32222/g' /etc/ssh/sshd_config
+sed -i 's/#Port 22/Port 51000/g' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
