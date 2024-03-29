39a943b3d315624d2012429af383393efb545767
linjicong <932997844@qq.com>
2024-03-29 15:17:57 +0800

    +

--- init-almalinux.sh
+++ init-almalinux.sh
@@ -10,7 +10,7 @@ sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 # 配置sshd
 if [ "$#" -ge 1 ]; then
   ssh_port=$1
-  sed -i 's/#Port 22/Port $ssh_port/g' /etc/ssh/sshd_config
+  sed -i 's/#Port 22/Port '$ssh_port'/g' /etc/ssh/sshd_config
 fi
 sed -i 's/#PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
